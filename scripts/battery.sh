78d735d4e2e474141b812572fb7b43c50f9ce4c3
Ingvar Kozlov <ikozlov11@gmail.com>
2017-01-29 19:58:27 -0800

    tmux statusline visual enhancements

--- scripts/battery.sh
+++ scripts/battery.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
+
 percentage=$(cat /sys/class/power_supply/BAT0/capacity)
 ac=$(cat /sys/class/power_supply/AC/online)
+
 if [[ $ac -eq 0 ]]; then
-    icon="❤"
+    icon='❤'
+    if [[ $percentage -ge 70 ]]; then
+        echo -n '#[fg=colour1]'
+    elif [[ $percentage -le 20 ]]; then
+        icon='☠'
+    else
+        echo -n '#[fg=colour2]'
+    fi
 else
-    icon="⚡"
+    icon='#[fg=colour3]⚡'
 fi
 
-#echo "♥ $percentage%"
-echo "$icon $percentage%"
+echo -n "$icon #[fg=colour7]$percentage%"
--- scripts/mpd_status.sh
+++ scripts/mpd_status.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 # a temporary hack to suppress the "Reading configuration from .." garbage
 # as per https://github.com/arybczak/ncmpcpp/issues/171
-now_playing=$(ncmpcpp --current-song "{%27t - %27a}|{%50f}" 2>/dev/null)
+now_playing=$(ncmpcpp --current-song "{%27t -- %27a}|{%50f}" 2>/dev/null)
+
 if [[ -n $now_playing ]]; then
-    echo "♪ $now_playing | "
+    echo -n "#[fg=colour6]♪ $now_playing #[default]#[fg=colour8]|#[default] "
 fi
