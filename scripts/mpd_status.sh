fd7aa07b1b461bec4ac6acdd32b9ddb5440ac91e
Igor Kozlov <ikozlov11@gmail.com>
2017-09-06 23:17:59 -0700

    Fix mpd in tmux statusline
    -fix current-song argument syntax
    -adjust tag length, <= 50
    -decrease length of mpd current song in statusline

--- scripts/mpd_status.sh
+++ scripts/mpd_status.sh
@@ -2,7 +2,7 @@
 
 # a temporary hack to suppress the "Reading configuration from .." garbage
 # as per https://github.com/arybczak/ncmpcpp/issues/171
-now_playing=$(ncmpcpp --current-song "{%27t -- %27a}|{%50f}" 2>/dev/null)
+now_playing=$(ncmpcpp --current-song="{%23t -- %18a}|{%45f}" 2>/dev/null)
 
 if [[ -n $now_playing ]]; then
     echo -n "#[fg=colour6]♪ $now_playing #[default]#[fg=colour8]|#[default] "
