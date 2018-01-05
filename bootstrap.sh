6484826130fa659b55d47d96a8db59e1f2236448
Igor Kozlov <ikozlov11@gmail.com>
2018-01-04 22:27:01 -0800

    add ranger and urxvt configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,9 +2,9 @@
 
 src=~/dotfiles
 dst=~
-files="bashrc gitconfig tmux.conf vimrc xinitrc Xresources config/redshift.conf config/mpd/mpd.conf config/ncmpcpp/config config/zathura/zathurarc"
+files="bashrc gitconfig tmux.conf vimrc xinitrc Xresources config/redshift.conf config/mpd/mpd.conf config/ncmpcpp/config config/ranger/rc.conf config/ranger/scope.sh config/zathura/zathurarc"
 
-mkdir -pv $dst/.config/{mpd,ncmpcpp,zathura}
+mkdir -pv $dst/.config/{mpd,ncmpcpp,ranger,zathura}
 
 for file in $files; do
     echo "Creating symlink at  $dst/.$file"
