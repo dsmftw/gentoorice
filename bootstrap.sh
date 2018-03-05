abd343346ab25168d3a911222488a241768ea60b
Igor Kozlov <ikozlov11@gmail.com>
2018-03-05 00:58:52 -0800

    Code rewrite /add conky & mpv
    -improve readability
    -fix syntax
    -add new configs

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-src=~/dotfiles
-dst=~
-files="bashrc gitconfig tmux.conf vimrc xinitrc Xresources config/redshift.conf config/mpd/mpd.conf config/ncmpcpp/config config/ranger/rc.conf config/ranger/scope.sh config/zathura/zathurarc"
+src="$HOME"/dotfiles
+dst="$HOME"
+declare -a files=(
+"bashrc" "gitconfig" "tmux.conf" "vimrc" "xinitrc" "Xresources"
+"config/redshift.conf"
+"config/conky/conky.conf"
+"config/mpd/mpd.conf"
+"config/mpv/mpv.conf"
+"config/ncmpcpp/config"
+"config/ranger/rc.conf"
+"config/ranger/scope.sh"
+"config/zathura/zathurarc"
+)
 
-mkdir -pv $dst/.config/{mpd,ncmpcpp,ranger,zathura}
+mkdir -pv "$dst"/.config/{conky,mpd,mpv,ncmpcpp,ranger,zathura}
 
-for file in $files; do
-    echo "Creating symlink at  $dst/.$file"
-    ln -S.bak -s $src/$file $dst/.$file
+printf "\nCreating symlinks...\n"
+for file in "${files[@]}"; do
+    printf '  %s/.%-25.25s --> dotfiles/%s\n' "$dst" "$file" "$file"
+    ln -S.bak -s "$src"/"$file" "$dst"/."$file"
 done
 
-echo "*** Done! ***"
+printf "\n***** Done! *****\n\n"
